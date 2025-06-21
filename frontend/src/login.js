eded2ade4f8928599bd924278af47f2944026e4d
ubc ubc <amypengyuehan@gmail.com>
2025-06-21 14:25:04 -0400

    new better and improved home page

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,11 +1,25 @@
-import './style.css'
+ import './style.css'
 import viteLogo from '/vite.svg'
 
+
 document.querySelector('#app').innerHTML = /*html*/ `
-<div id="logo"></div>
-<form id="login">
-    <input type="text" placeholder="Username" class="input input-xl" "id="username />
+  React.createElement(
+      'div',
+      {
+        className: 'bg-white rounded-lg overflow-hidden',
+        style: {
+          width: '360px',
+          height: '780px',
+          border: 'none',
+        }
+        <div id="logo">
+    <img src="public/logo.png" alt="Logo" />
+  </div>
+
+  <form id="login">
+    <input type="text" placeholder="Username" class="input input-xl" id="username" />
     <input type="password" placeholder="Password" class="input input-xl" id="password" />
     <button class="btn btn-primary btn-xl" id="submit">Enter</button>
-<form>
+  </form>
 `
+
