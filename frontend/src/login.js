9d1d332491d2971360c2886d96d00833927e5d08
Ananth Arunkumar <ananth2.718@gmail.com>
2025-06-21 06:12:07 -0400

    minor changes

---
+++ frontend/src/login.js
@@ -0,0 +1,15 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = /*html*/ `
+<div id="logo"></div>
+<form id="login">
+    <input type="text" placeholder="Username" class="input input-xl" id="username" />
+    <input type="password" placeholder="Password" class="input input-xl" id="password" />
+    <button class="btn btn-primary btn-xl" id="submit">Enter</button>
+<form>
+`
+
+setupCounter(document.querySelector('#counter'))
