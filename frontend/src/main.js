9155db767fe418d8d55594e5edde5bf6aae550d4
Ananth Arunkumar <ananth2.718@gmail.com>
2025-06-21 02:41:39 -0400

    frontend setup

---
+++ frontend/src/main.js
@@ -0,0 +1,10 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = /*html*/ `
+  
+`
+
+setupCounter(document.querySelector('#counter'))
